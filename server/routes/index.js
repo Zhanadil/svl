d5786118d8eda95b9ad9ddb95139d97ff0cff778
Zhanadil Nurtoleuov <znurtoleuov@gmail.com>
2018-12-05 17:17:22 +0600

    admin validation added

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const generalRouter = require('@routes/general');
 const userRouter = require('@routes/user');
+const adminRouter = require('@routes/admin');
 
 router.use('/general', generalRouter);
 router.use('/user', userRouter);
+router.use('/admin', adminRouter);
 
 module.exports = router;
